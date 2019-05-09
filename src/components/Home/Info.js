4201830f393b215c1f8055cbcf35a397b563d39e
bdmllc <beachdaymedia@gmail.com>
2019-05-09 18:24:42 -0400

    first commit

---
+++ src/components/Home/Info.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function info() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum
+              ea rerum totam alias fuga perspiciatis veniam, eligendi,
+              recusandae, dignissimos id tempore soluta! Libero asperiores sed
+              id debitis cupiditate perspiciatis explicabo laudantium nemo,
+              corporis quos quis quia dignissimos saepe ipsam optio enim fuga
+              minus illo. Quasi iure repellendus fugiat repellat similique.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
