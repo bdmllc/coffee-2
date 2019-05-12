ee32dcb127c871afd96267a98e5aeb3ce387edc3
bdmllc <beachdaymedia@gmail.com>
2019-05-12 15:06:56 -0400

    another commit

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <Title title="contact us" />
       <div className="row">
         <div className="col-10 col-sm-8 col-md-6 mx-auto">
-          <form name="contact" method="POST" data-netlify="true">
+          <form action="https://formspree.io/rickpr44@gmail.com" method="POST">
             {/* { name } */}
             <div className="form-group">
               <label htmlFor="name">Name</label>
